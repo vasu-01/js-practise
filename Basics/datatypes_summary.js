16cce2f3aaa2904c137b8ff14b5b9bbd2edd2981
Vishal Kumar <122470693+vasu-01@users.noreply.github.com>
2025-06-24 07:21:50 +0000

    datatypes and memory summary

---
+++ Basics/datatypes_summary.js
@@ -0,0 +1,14 @@
+// #Primitive data types
+// String, Number, Bigint, Boolean, null, undefined, Symbol
+
+
+
+
+// #Reference type/ Non- primitive datatypes
+// Arrays, Objects, Functions
+
+
+///////////////////////////////////// Memory//////////////////////////
+
+//1. Stack (Primitive): copy of variable we got 
+//2. Heap (Non-Primitive): reference  we got
